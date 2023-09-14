afded2212adb090749b42e31ba5216c94e1ac521
Kanay <kanosss1@gmail.com>
2023-09-14 23:12:30 +0600

    first view

---
+++ src/page/Main/Main.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import main from "./Main.module.css";
+
+const Main = () => {
+  return (
+    <>
+      <h2 className={main.txt}>Main Page</h2>
+    </>
+  );
+};
+
+export default Main;
